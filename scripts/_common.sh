f237277d78b8ddd4a0eb4d77cbf522b597be71aa
Rafi594 <rafi59_dev@srvmaison.fr.nf>
2019-10-22 17:52:18 +0200

    Remove postgresql support

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql9.6 postgresql-contrib build-essential libirrlicht-dev cmake libbz2-dev libpng-dev libjpeg-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libluajit-5.1-dev"
+pkg_dependencies="build-essential libirrlicht-dev cmake libbz2-dev libpng-dev libjpeg-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libluajit-5.1-dev"
 
 #=================================================
 # PERSONAL HELPERS
